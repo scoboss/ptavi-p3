816580dad5109dfda3b5e96d98fddd277876b374
Sandra Cobos Sacido <sandra.12@outlook.es>
2016-10-04 12:08:54 +0200

    Avance P3

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -5,3 +5,17 @@
 Practica 3
 
 """
+from xml.sax.handler import ContentHandler
+
+
+class SmallSMILHandler(ContentHandler):
+        def __init__(self):
+                self.lista_etiquetas = []
+                self.dicc = {'root-layout': ['width' ,'height' ,
+                        'background-color'], 
+                        'region': ['id', 'top', 'bottom', 'left', 'right'],
+                        'img': ['src', 'region', 'begin', 'dur'],
+                        'audio': ['src', 'begin', 'dur'],
+                        'textstream': ['src', 'region']}
+
+        
