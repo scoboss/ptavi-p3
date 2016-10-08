ac3a27a4d2c83e649fa5a7b66fdf55b08c9a5a85
Sandra Cobos Sacido <sandra.12@outlook.es>
2016-10-08 13:43:53 +0200

    Modificamos Small

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -6,12 +6,16 @@ Practica 3
 
 """
 from xml.sax.handler import ContentHandler
+from xml.sax import make_parser
 
 
 class SmallSMILHandler(ContentHandler):
+        """
+        Inicializamos.
+        """
         def __init__(self):
                 self.lista_etiquetas = []
-                self.dicc = {'root-layout': ['width' ,'height' ,
+                self.dic = {'root-layout': ['width' ,'height' ,
                         'background-color'], 
                         'region': ['id', 'top', 'bottom', 'left', 'right'],
                         'img': ['src', 'region', 'begin', 'dur'],
@@ -19,12 +23,12 @@ class SmallSMILHandler(ContentHandler):
                         'textstream': ['src', 'region']}
         
         def startElement(self, name, attrs):
-                if name in self.dicc:
-                        dicc = {}
-                for item in self.dicc[name]:
-                        dicc[item] = attrs.get(item,"")
-                        diccname = {name: dicc}
-                        self.lista_etiquetas.append(diccname)
+            if name in self.dic:
+                dicc = {}
+            for item in self.dic[name]:
+                dicc[item] = attrs.get(item,"")
+            diccname = {name: dicc}
+            self.lista_etiquetas.append(diccname)
 
         """
         Devuelve la lista con las etiquetas encontradas
@@ -32,8 +36,9 @@ class SmallSMILHandler(ContentHandler):
         def get_tags(self):
                 return self.lista_etiquetas
 
-        if __name__ == "__main__":
-                print ("hola")
-        
-
-        
+if __name__ == "__main__":
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('karaoke.smil'))
+    print (cHandler.get_tags())        
